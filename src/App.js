006c4079957908a18cd48cc7de8d05bac6adf7de
Jeff Webb <webbwear@icloud.com>
2019-01-28 12:09:42 -0500

    onClick showPersons

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
       { name: 'Kathleen', age: 45 },
       { name: 'Anastasia', age: 10 }
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
@@ -33,24 +34,33 @@ class App extends Component {
   } )
   }
 
+  togglePersonsHandler = () => {
+      const doesShow = this.state.showPersons;
+      this.setState({showPersons: !doesShow});
+  }
+
   render() {
     return (
       <div className="App">
         <h1>My New React App</h1>
-        <button onClick={this.switchNameHandler.bind(this, 'Donkey')}>Switch Name</button>
-        <Person 
-          name={this.state.persons [0].name} 
-          age={this.state.persons [0].age} />
-        <Person 
-          name={this.state.persons [1].name} 
-          age={this.state.persons [1].age}
-          click={this.switchNameHandler.bind(this, 'Donkey Daddy!!!')}
-          changed={this.nameChangedHandler} >Is Cool!</Person>
-        <Person 
-          name={this.state.persons [2].name} 
-          age={this.state.persons [2].age}>Is the Best...ist Daughter!!!!</Person>
+        <button 
+          onClick={this.togglePersonsHandler}>Switch Name</button>
+      { this.state.showPersons ? 
+        <div>
+          <Person 
+            name={this.state.persons [0].name} 
+            age={this.state.persons [0].age} />
+          <Person 
+            name={this.state.persons [1].name} 
+            age={this.state.persons [1].age}
+            click={this.switchNameHandler.bind(this, 'Donkey Daddy!!!')}
+            changed={this.nameChangedHandler} >Is Cool!</Person>
+          <Person 
+            name={this.state.persons [2].name} 
+            age={this.state.persons [2].age}>Is the Best...ist Daughter!!!!</Person>
 
-
+        </div> : null
+      }   
       </div>
     );
   }
